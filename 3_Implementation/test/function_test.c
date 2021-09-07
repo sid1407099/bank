c039f8152a3719c4c20288f1e0160f34c2ce304d
sid1407099 <52995532+sid1407099@users.noreply.github.com>
2021-09-07 12:54:26 +0530

    Update function_test.c

--- 3_Implementation/test/function_test.c
+++ 3_Implementation/test/function_test.c
@@ -1,1 +1,65 @@
+/**
+ * @file function_test.c
+ * @author siddharth.jain@ltts.com
+ * @brief 
+ * @version 0.1
+ * @date 2021-09-06
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include "function.h"
+#include "unity.h"
+#include<stdlib.h>
 
+
+
+void setUp(){
+
+}
+void tearDown(){
+
+}
+void function_create(){
+    
+creation();
+}
+
+void function_depo(){
+    deposit();
+}
+
+void function_with(){
+    withdraw();
+}
+
+void function_bal(){
+     bal();
+}
+
+void function_ex(){
+     exit(0);
+}
+
+
+
+
+int main(void){
+    UNITY_BEGIN();
+    RUN_TEST(function_depo); //invalid
+
+    RUN_TEST(function_bal); //invalid
+
+    RUN_TEST(function_depo); //invalid
+
+    RUN_TEST(function_with); //invalid 
+
+    RUN_TEST(function_create);
+    RUN_TEST(function_depo);
+    RUN_TEST(function_with);
+    RUN_TEST(function_bal);
+    RUN_TEST(function_with); // cant withdraw all 
+
+    RUN_TEST(function_ex);
+    return UNITY_END();
+}
